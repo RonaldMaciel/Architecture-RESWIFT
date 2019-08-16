21adc47a936756b1c44793bc407924af9c7b5d79
Ronald Maciel <ronaldsmaciel2@outlook.com>
2019-08-16 17:25:12 -0300

    Start the request from each API (nowPlaying, popularMovies, details) and improved the architecture

---
+++ TheMovieDB/Reducers/PopularMoviesReducer.swift
@@ -0,0 +1,14 @@
+//
+//  PopularMoviesReducer.swift
+//  TheMovieDB
+//
+//  Created by Ronald Maciel on 16/08/19.
+//  Copyright © 2019 Ronald Maciel. All rights reserved.
+//
+
+import ReSwift
+
+func popularMoviesReducer(action: Action, state: PopularMoviesState?) -> PopularMoviesState {
+    var state = state ?? PopularMoviesState(table: .loading)
+    return state
+}
